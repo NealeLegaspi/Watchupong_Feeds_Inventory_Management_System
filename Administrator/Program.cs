fa29ca54a11c6e4fe7a7b725400bf73f171c8cf3
Jessie D. Samaniego <150759974+TzuYeSoo@users.noreply.github.com>
2024-11-28 17:01:31 +0800

    Recovering the git

--- Administrator/Program.cs
+++ Administrator/Program.cs
@@ -11,11 +11,9 @@ namespace Administrator
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-<<<<<<< HEAD
+
             Application.Run(new Admin_Login());
-=======
             Application.Run(new Form1());
->>>>>>> a1e045f660a91dfd927fe6c458441b8e8e21e2dd
         }
     }
 }
